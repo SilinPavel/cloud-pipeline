ff26a46022ae4446ae818fbf659ca90e8929d277
Evgenii Mukhamadiarov <e.mukhamadiarov@gmail.com>
2019-08-20 16:21:29 +0300

    updated entities implementation. Removed constructors from enum-classes. Added @AllArgsConstructor and @ToString annotations for them.

--- tes-adapter/src/main/java/com/epam/pipeline/tesadapter/entity/TesFileType.java
+++ tes-adapter/src/main/java/com/epam/pipeline/tesadapter/entity/TesFileType.java
@@ -3,18 +3,19 @@ package com.epam.pipeline.tesadapter.entity;
 import com.fasterxml.jackson.annotation.JsonValue;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.ToString;
 
 /**
  * Gets or Sets tesFileType
  */
+@AllArgsConstructor
+@ToString
 public enum TesFileType {
     FILE("FILE"),
     DIRECTORY("DIRECTORY");
 
     private String value;
-
-    TesFileType(String value) {
-        this.value = value;
-    }
 }
 
